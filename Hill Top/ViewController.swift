370de8f3838475ac7b926182cb7d6756c70d71f7
Justin Keller <jdk370@gmail.com>
2017-09-25 07:26:45 -0400

    Initial Commit

---
+++ Hill Top/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Hill Top
+//
+//  Created by Justin Keller on 9/25/17.
+//  Copyright © 2017 Justin Keller. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
