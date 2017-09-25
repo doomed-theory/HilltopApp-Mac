1f4391acdb801716a075f7f6666b46e03f2c0588
Justin Keller <jdk370@gmail.com>
2017-09-25 13:43:42 -0400

    initial commit

---
+++ Hill Top/TeacherViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Hill Top
+//
+//  Created by Justin Keller on 9/25/17.
+//  Copyright © 2017 Justin Keller. All rights reserved.
+//
+
+import Cocoa
+
+class TeacherViewController: NSViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //Teacherbutton.font=NSFont(name: "Times New Roman", size: 26)
+        //StudentButton.font=NSFont(name: "Times New Roman", size: 26)
+        // Do any additional setup after loading the view.
+    }
+    
+    override var representedObject: Any? {
+        didSet {
+            // Update the view, if already loaded.
+        }
+    }
+    
+    
+}
+
