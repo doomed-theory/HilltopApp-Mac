409bce0156a329f27d34293a83f46a05646d2324
Justin Keller <jdk370@gmail.com>
2017-10-06 13:03:21 -0400

    added super

--- Hill Top/credits.swift
+++ Hill Top/credits.swift
@@ -11,7 +11,8 @@ import AppKit
 
 class creditsClass :NSViewController {
     @IBOutlet var myMainMan: NSTextField!
-    override func viewDidLoad() {
+    override func viewWillAppear() {
+        super.viewWillAppear()
         myMainMan.stringValue="Justin Keller, C)PTE, ACTC, ACMT, ACSP -- Lead Developer, Project Manager\n"
         myMainMan.stringValue+="Matthew Kleiner -- Developer\n"
         myMainMan.stringValue+="Ryan Wellock -- Chief Designer"
