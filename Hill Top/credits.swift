d187900e29fd030fa0b693191a79aac496cd6f36
Justin Keller <jdk370@gmail.com>
2017-10-06 13:10:09 -0400

    fixed text label

--- Hill Top/credits.swift
+++ Hill Top/credits.swift
@@ -13,9 +13,7 @@ class creditsClass :NSViewController {
     @IBOutlet var myMainMan: NSTextField!
     override func viewWillAppear() {
         super.viewWillAppear()
-        myMainMan.stringValue="Justin Keller, C)PTE, ACTC, ACMT, ACSP -- Lead Developer, Project Manager\n"
-        myMainMan.stringValue+="Matthew Kleiner -- Developer\n"
-        myMainMan.stringValue+="Ryan Wellock -- Chief Designer"
+        myMainMan.stringValue="Justin Keller, C)PTE, ACTC, ACMT, ACSP -- Lead Developer, Project Manager\n"+"Matthew Kleiner -- Developer\n"+"Ryan Wellock -- Chief Designer"
         
     }
 }
